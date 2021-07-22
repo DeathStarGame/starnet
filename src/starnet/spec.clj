ff9f91a78efcdb0ccb20f5efb7c2758bf03e1891
Sergei Udris <sergei.udris@gmail.com>
2021-07-22 20:30:44 +0300

    remove kafka, http-server, buddy, keys from src

--- src/starnet/spec.clj
+++ src/starnet/spec.clj
@@ -2,38 +2,9 @@
   (:require
    [clojure.spec.alpha :as s]
    [clojure.spec.gen.alpha :as gen]
-   [clojure.spec.test.alpha :as stest])
-  (:import
-   java.util.Date
-   org.apache.kafka.clients.producer.KafkaProducer
-   org.apache.kafka.clients.consumer.KafkaConsumer))
+   [clojure.spec.test.alpha :as stest]))
 
 (derive java.util.Map :isa/map)
 (derive java.util.Set :isa/set)
 (derive java.util.UUID :isa/uuid)
-(derive org.apache.kafka.clients.producer.KafkaProducer :isa/kproducer)
-(derive org.apache.kafka.clients.consumer.KafkaConsumer :isa/kconsumer)
 (derive clojure.lang.Keyword :isa/keyword)
-
-(s/def :instance/kproducer #(instance? org.apache.kafka.clients.producer.KafkaProducer %))
-
-(s/def :instance/kconsumer #(instance? org.apache.kafka.clients.consumer.KafkaConsumer %))
-
-
-(s/fdef starnet.streams/produce-event
-  :args (s/cat :producer :instance/kproducer
-               :topic string?
-               :k (s/alt :uuid uuid? :string string?)
-               :event :ev/event))
-
-(s/fdef starnet.streams/next-state-user
-  :args (s/cat :state (s/nilable :u/user)
-               :k uuid?
-               :ev :ev.u/event)
-  :ret (s/nilable :u/user))
-
-(s/fdef starnet.streams/next-game
-  :args (s/cat :state (s/nilable :g/game)
-               :k uuid?
-               :ev :ev.g/event )
-  :ret (s/nilable :g/game))
